eac3446782f17713b988044611b951a53ada1c72
Maro <54935373+maro-03@users.noreply.github.com>
2022-04-19 13:18:30 +0200

    changes

--- H02/index.php
+++ H02/index.php
@@ -1,6 +1,31 @@
 <?php
-$a = "Ik heb niet gefaald. Ik heb alleen ";
-$b = "10000 manieren gevonden ";
-$c = "die niet werken.";
+// Thomas Edison
+$a = "Ik heb niet gefaald";
+$b = "die niet werken.";
+$c = 10000;
 
-echo $a.$b.$c;
+    echo $a.", ik heb alleen ".$c." manieren gevonden ".$b;
+        echo "<br>";
+
+// Albert Plesman
+$d = "die geen fout";
+$e = "en";
+$f = "niet";
+
+    echo " Mensen ".$d.$e." maken, werken ".$f.".";
+        echo "<br>";
+
+// Tyra Banks
+$g = "ry";
+$h = ", but";
+$i = "fail";
+
+    echo "T".$g." and fail".$h." never ".$i." to try.";
+        echo "<br>";
+
+// Herman Finkers
+$j = "\"omgaan";
+$k = "met";
+$l = "teleu";
+
+    echo "De cursus ".$j." ".$k." ".$l."rstellingen\" kan vanavond helaas niet doorgaan.";
