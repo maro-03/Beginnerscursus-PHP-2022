749c6f3a08f1ec142c6a9e80eff9473117fa7fd1
Maro <54935373+maro-03@users.noreply.github.com>
2022-04-21 10:49:53 +0200

    changes

--- H03/aaploop.php
+++ H03/aaploop.php
@@ -11,15 +11,42 @@
             .red {
                 border:red solid 6px;
             }
+            .green {
+                border:green solid 6px;
+            }
         </style>
 </head>
 <body>
     <?php
     for($i = 1; $i<=9 ;$i++) {
-        if($i == 3){
-            $style = "class='rood'";
+        if($i == 2) {
+            $style = "class='red'";
         }   else {
             $style = "";
+        }
+        if($i == 4) {
+            $style = "class='red'";
+        }
+        if($i == 6) {
+            $style = "class='red'";
+        }
+        if($i == 8) {
+            $style = "class='red'";
+        }
+        if($i == 1) {
+            $style = "class='green'";
+        }
+        if($i == 3) {
+            $style = "class='green'";
+        }
+        if($i == 5) {
+            $style = "class='green'";
+        }
+        if($i == 7) {
+            $style = "class='green'";
+        }
+        if($i == 9) {
+            $style = "class='green'";
         }
         echo "<img ".$style."src='img/aap".$i.".jpg'>";
     }
