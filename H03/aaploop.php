d861d5d3264a638fbf29d5ea7814a3b64bb66cad
Maro <54935373+maro-03@users.noreply.github.com>
2022-04-21 11:15:11 +0200

    changes

--- H03/aaploop.php
+++ H03/aaploop.php
@@ -19,33 +19,9 @@
 <body>
     <?php
     for($i = 1; $i<=9 ;$i++) {
-        if($i == 2) {
+        if($i %  2 == 0) {
             $style = "class='red'";
         }   else {
-            $style = "";
-        }
-        if($i == 4) {
-            $style = "class='red'";
-        }
-        if($i == 6) {
-            $style = "class='red'";
-        }
-        if($i == 8) {
-            $style = "class='red'";
-        }
-        if($i == 1) {
-            $style = "class='green'";
-        }
-        if($i == 3) {
-            $style = "class='green'";
-        }
-        if($i == 5) {
-            $style = "class='green'";
-        }
-        if($i == 7) {
-            $style = "class='green'";
-        }
-        if($i == 9) {
             $style = "class='green'";
         }
         echo "<img ".$style."src='img/aap".$i.".jpg'>";
---
+++ H03/busreis.php
@@ -0,0 +1,24 @@
+<?php
+?>
+<!DOCTYPE html>
+<head>
+<title>Busreis</title>
+</head>
+<body>
+    <?php
+    $leeftijd = 2;
+    $prijs = 10;
+    if($leeftijd > 65)  {
+        $prijs = $prijs * 0.5;
+    }
+    if($leeftijd <= 12) {
+        $prijs = $prijs * 0.5;
+    }
+    if($leeftijd < 3)   {
+        $prijs = 0;
+    }
+    echo $prijs;
+
+    ?>
+</body>
+</html>
